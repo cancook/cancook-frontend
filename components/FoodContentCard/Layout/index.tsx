6bab39758ecd2729107661f386377cc662b00df3
Yoon-Hae-Min <yunhatmi@naver.com>
2023-04-21 22:44:42 +0900

    refactor: change export method

--- components/FoodContentCard/Layout/index.tsx
+++ components/FoodContentCard/Layout/index.tsx
@@ -1,3 +1,5 @@
 import styled from '@emotion/styled';
 
-export const FoodContentCardLayout = styled.article``;
+const FoodContentCardLayout = styled.article``;
+
+export default FoodContentCardLayout;
