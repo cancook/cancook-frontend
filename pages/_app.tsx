fc61f2e827a557f773bb88a97c3650d603144729
donghakang <dkang0602@gmail.com>
2023-04-23 17:37:20 +0900

    feat: swiper.js 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,18 @@ import { Global, ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
 import { global } from '@/styles/Global';
 import ReactQueryProvider from '@/components/react-query/ReactQueryProvider';
+import Layout from '@/components/layout/Layout';
+// Swiper
+import 'swiper/css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ReactQueryProvider>
       <ThemeProvider theme={theme}>
         <Global styles={global} />
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </ThemeProvider>
     </ReactQueryProvider>
   );
