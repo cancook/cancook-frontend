ee70e2de6f2dcc681fec204bcf30a0fd114ae947
Yoon-Hae-Min <yunhatmi@naver.com>
2023-05-30 21:41:33 +0900

    feat: axios instance 생성

---
+++ apis/axios.ts
@@ -0,0 +1,51 @@
+import axios, { AxiosError } from 'axios';
+
+export const BASE_URL =
+  process.env.NODE_ENV === 'production' ? 'http://self-dining.shop' : '/api';
+
+const axiosClient = (() => {
+  const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    timeout: 3000,
+    responseType: 'json',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  axiosInstance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error: AxiosError) => {
+      if (error.code === 'ECONNABORTED') {
+        alert('서버에 이상이 있습니다.');
+      }
+      switch (error.response?.status) {
+        case 401:
+          console.warn('401: 인증정보 없음');
+          break;
+        case 403:
+          console.warn('403: 권한 없음');
+          break;
+        case 404:
+          console.warn('404: 대상을 찾지 못함');
+          break;
+        case 500:
+        case 501:
+        case 502:
+        case 503:
+        case 504:
+          console.warn(`${error.response?.status}: 서버 오류`);
+          break;
+        default:
+          console.error(`${error.response?.status}: unhandled error!`);
+          break;
+      }
+      return Promise.reject(error);
+    }
+  );
+  return axiosInstance;
+})();
+
+export default axiosClient;
