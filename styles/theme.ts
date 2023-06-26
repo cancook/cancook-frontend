ab20c7894216da3aec74d783561e893ebb9b9c06
donghakang <dkang0602@gmail.com>
2023-06-27 00:22:14 +0900

    fix: global theme screen breakpoint

--- styles/theme.ts
+++ styles/theme.ts
@@ -139,24 +139,10 @@ export const font = {
 
 // const colors = {}
 export const screen = {
-  /* Extra small devices (phones, 600px and down) */
-  xs: '@media only screen and (max-width: 600px)',
-
-  /* Small devices (portrait tablets and large phones, 600px and up) */
-  s: '@media only screen and (min-width: 600px)',
-
-  /* Medium devices (landscape tablets, 768px and up) */
-  m: '@media only screen and (min-width: 768px)',
-
-  /* Large devices (laptops/desktops, 992px and up) */
-  l: '@media only screen and (min-width: 992px)',
-
-  /* Extra large devices (large laptops and desktops, 1200px and up) */
-  xl: '@media only screen and (min-width: 1200px)',
-
   /** All Devices  */
-  phone: '@media only screen and (max-width: 768px)',
-  monitor: '@media only screen and (min-width: 768px)'
+  phone: '@media only screen and (max-width: 767px)',
+  tablet: '@media only screen and (min-width: 768px) and (max-width: 1439px)',
+  monitor: '@media only screen and (min-width: 1440px)'
 };
 
 const theme: Theme = {
