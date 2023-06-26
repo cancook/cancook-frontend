fa89864731322ec491ce80203000b07eba770243
donghakang <dkang0602@gmail.com>
2023-06-27 01:03:54 +0900

    feat: initial banner update

--- components/Carousel/Banner/BannerCarousel.tsx
+++ components/Carousel/Banner/BannerCarousel.tsx
@@ -1,46 +1,37 @@
 import React from 'react';
 
-import FoodContentCard from '@/components/FoodContentCard';
+import { Banner } from '@/types/banner';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Navigation } from 'swiper';
+import { Autoplay, Navigation, Pagination } from 'swiper';
 import 'swiper/css/navigation';
 import styled from '@emotion/styled';
-import { YoutubeRecommended } from '@/types/youtube';
+import Image from 'next/image';
 
 export type BannerCarouselProps = {
-  /**Thumbnail 콘텐츠들 */
-  contents: YoutubeRecommended[] | undefined;
+  banners: Banner[];
   isLoading: boolean;
 };
 
-const BannerCarousel = ({ contents, isLoading }: BannerCarouselProps) => {
+const BannerCarousel = ({ banners, isLoading }: BannerCarouselProps) => {
   return (
     <BannerContainer>
       <Swiper
-        modules={[Autoplay, Navigation]}
-        spaceBetween={20}
-        slidesPerView={2}
-        centeredSlides={true}
+        modules={[Autoplay, Navigation, Pagination]}
+        slidesPerView={1}
         navigation
+        pagination
         autoplay={{ delay: 3000, pauseOnMouseEnter: true }}
         loop
       >
-        {contents?.map((content) => (
-          <SwiperSlide
-            key={`content-${content.id}`}
-            style={{
-              transform: 'none'
-              // z-index를 사용하려면 기존에 있는 transform속성을 초기화 시켜야 사용할수 있음 동시에 사용하면 z-index가 무시됨
-            }}
-          >
-            <ImageContainer
-              onClick={() => window.open(content.link, '__blank')}
-            >
-              <FoodContentCard.Thumbnail
-                time={content.playTime}
-                src={content.thumbnailURL}
-                size="lg"
+        {banners.map((banner: Banner, idx: number) => (
+          <SwiperSlide key={`banner-${idx}`}>
+            <ImageContainer background={banner.background}>
+              <Image
+                src={banner.img.phone}
+                alt={banner.description}
+                fill
+                style={{ objectFit: 'cover' }}
               />
             </ImageContainer>
           </SwiperSlide>
@@ -53,25 +44,28 @@ const BannerCarousel = ({ contents, isLoading }: BannerCarouselProps) => {
 export default BannerCarousel;
 
 const BannerContainer = styled.div`
-  min-width: 78.75rem;
   .swiper {
     overflow: visible;
     .swiper-button-next,
     .swiper-button-prev {
       color: ${({ theme }) => theme.colors.white[50]};
+      &::after {
+        font-size: 2rem;
+      }
     }
   }
 `;
 
-const ImageContainer = styled.div`
+const ImageContainer = styled.div<{ background: string }>`
   position: relative;
   overflow: visible;
-  aspect-ratio: 16 / 9;
-  width: 100%;
+  background-color: ${({ background }) => background};
+
+  height: 160px;
+
   transition: all 100ms linear;
-  &:hover {
-    scale: 1.2;
-    box-shadow: 0 0 4rem 2rem rgba(0, 0, 0, 0.2);
-    z-index: 50;
+
+  ${({ theme }) => theme.screen.m} {
+    height: 340px;
   }
 `;
---
+++ types/banner.ts
@@ -0,0 +1,5 @@
+export type Banner = {
+  background: string;
+  img: { default: string; phone: string };
+  description: string;
+};
