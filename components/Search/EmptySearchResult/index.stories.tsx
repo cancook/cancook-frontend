f823deacea669e7ac032848578349118fc525fff
YoonHaeMin <yunhatmi@gachon.ac.kr>
2023-06-30 12:57:31 +0900

    feat: 빈 검색 결과 컴포넌트 생성 (#37)
    * style: breakPoint 수정
    
    * feat: 빈 검색 결과 UI생성
    
    * style: 리뷰 반영

---
+++ components/Search/EmptySearchResult/index.stories.tsx
@@ -0,0 +1,18 @@
+import { StoryFn } from '@storybook/react';
+import EmptySearchResult from '.';
+import { ComponentProps } from 'react';
+
+const EmptySearchResultComponent = {
+  title: 'Component/Search/EmptySearchResult',
+  component: EmptySearchResult
+};
+export default EmptySearchResultComponent;
+
+type EmptySearchResultProps = ComponentProps<typeof EmptySearchResult>;
+
+/**
+ * 검색 결과가 없을 때 보여주는 컴포넌트입니다.
+ */
+export const EmptySearchResultTemplate: StoryFn<EmptySearchResultProps> = (
+  args
+) => <EmptySearchResult />;
---
+++ components/Search/EmptySearchResult/index.tsx
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled';
+import Image from 'next/image';
+import React from 'react';
+import emptyGIFPath from '@/public/gifs/stir-cook.gif';
+
+const Wrapper = styled.div`
+  width: 11rem;
+  height: 8.5rem;
+  ${({ theme }) => theme.screen.desktop} {
+    width: 19.3125rem;
+    height: 21rem;
+  }
+`;
+const Text = styled.p`
+  color: ${({ theme }) => theme.colors.gray[400]};
+  text-align: center;
+  ${({ theme }) => theme.font.body.md};
+  ${({ theme }) => theme.screen.desktop} {
+    ${({ theme }) => theme.font.title.lg};
+    line-height: 2.125rem;
+  }
+`;
+
+const EmptyResultImage = styled(Image)`
+  display: block;
+  margin: auto;
+  width: 5rem;
+  height: 5rem;
+
+  ${({ theme }) => theme.screen.desktop} {
+    width: 16.25rem;
+    height: 16.25rem;
+  }
+`;
+
+const EmptySearchResult = () => {
+  return (
+    <Wrapper>
+      <EmptyResultImage src={emptyGIFPath} alt="empty search image" />
+      <Text>
+        자고 있는 재료들을 깨워 <br />
+        만들수 있는 레시피를 찾고 있어요.
+      </Text>
+    </Wrapper>
+  );
+};
+
+export default EmptySearchResult;
