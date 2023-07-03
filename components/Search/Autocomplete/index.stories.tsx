0ed2d390247615e54ee3477e97ecdd7206667ebd
YoonHaeMin <yunhatmi@gachon.ac.kr>
2023-07-03 21:04:11 +0900

    fix: 컴포넌트 변경에 따른 스토리북 빌드 변경 수정 (#49)

--- components/Search/Autocomplete/index.stories.tsx
+++ components/Search/Autocomplete/index.stories.tsx
@@ -1,5 +1,5 @@
 import { StoryFn } from '@storybook/react';
-import { ComponentProps } from 'react';
+import { ComponentProps, useState } from 'react';
 import Autocomplete from '.';
 import SearchInput from '../SearchInput';
 
@@ -14,26 +14,42 @@ type AutocompleteProps = ComponentProps<typeof Autocomplete>;
 /**
  * 검색시 자동 완성 토글 컴포넌트입니다.
  */
-export const AutocompleteTemplate: StoryFn<AutocompleteProps> = (args) => (
-  <div
-    style={{
-      maxWidth: '50rem',
-      margin: 'auto'
-    }}
-  >
-    <SearchInput />
-    <Autocomplete
-      keywords={[
-        '키워드1',
-        '키워드2',
-        '키워드3',
-        '키워드4',
-        '키워드5',
-        '키워드6',
-        '키워드7',
-        '키워드8',
-        '키워드9'
-      ]}
-    />
-  </div>
-);
+export const AutocompleteTemplate: StoryFn<AutocompleteProps> = (args) => {
+  const [isFocus, setIsFocus] = useState(true);
+
+  const handleFocus = () => {
+    setIsFocus(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocus(false);
+  };
+  return (
+    <div
+      style={{
+        maxWidth: '50rem',
+        margin: 'auto'
+      }}
+    >
+      <SearchInput
+        isFocus={isFocus}
+        handleFocus={handleFocus}
+        handleBlur={handleBlur}
+      />
+      <Autocomplete
+        keywords={[
+          '키워드1',
+          '키워드2',
+          '키워드3',
+          '키워드4',
+          '키워드5',
+          '키워드6',
+          '키워드7',
+          '키워드8',
+          '키워드9'
+        ]}
+        isOpen={isFocus}
+      />
+    </div>
+  );
+};
