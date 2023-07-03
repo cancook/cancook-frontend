8d394e4d293bf7705d8c17a76c03c7d0ddcc8730
Yoon-Hae-Min <yunhatmi@naver.com>
2023-07-03 13:15:06 +0900

    fix: 컴포넌트 변경에 따른 스토리북 빌드 변경 수정

--- components/Search/SearchInput/index.stories.tsx
+++ components/Search/SearchInput/index.stories.tsx
@@ -1,5 +1,5 @@
 import { StoryFn } from '@storybook/react';
-import { ComponentProps } from 'react';
+import { ComponentProps, useState } from 'react';
 import SearchInput from '.';
 
 const SearchInputComponent = {
@@ -13,9 +13,24 @@ type SearchInputProps = ComponentProps<typeof SearchInput>;
 /**
  * 메인 검색 input 컴포넌트 입니다.
  */
-export const SearchInputTemplate: StoryFn<SearchInputProps> = (args) => (
-  <SearchInput {...args} />
-);
+export const SearchInputTemplate: StoryFn<SearchInputProps> = (args) => {
+  const [isFocus, setIsFocus] = useState(true);
+
+  const handleFocus = () => {
+    setIsFocus(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocus(false);
+  };
+  return (
+    <SearchInput
+      isFocus={isFocus}
+      handleFocus={handleFocus}
+      handleBlur={handleBlur}
+    />
+  );
+};
 // SearchInputTemplate.args = {
 //   src: 'https://i.ytimg.com/vi/B2s5seuBCn0/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLArD-nM9VEHz5TZ3qai6BzHy9H28A',
 //   children: '김코딩'
