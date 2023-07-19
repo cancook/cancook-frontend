33cf847af0f5c174583a4603674fda40a15be095
donghakang <dkang0602@gmail.com>
2023-07-19 21:32:42 +0900

    fix: checkbox responsive

--- components/common/Checkbox/index.tsx
+++ components/common/Checkbox/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-import CheckIcon from '@/public/svg/check.svg';
+import Image from 'next/image';
 
 export interface CheckboxFieldProps {
   className?: string;
@@ -10,7 +10,6 @@ export interface CheckboxFieldProps {
   label?: string;
   checked: boolean;
   setValue: () => void;
-  link?: string;
 }
 
 const CheckboxField = ({
@@ -19,14 +18,12 @@ const CheckboxField = ({
   id,
   label,
   checked,
-  setValue,
-  link
+  setValue
 }: CheckboxFieldProps) => {
   return (
     <Field className={className}>
-      <div className="checkbox-wrapper">
-        <input
-          className="radio"
+      <CheckboxWrapper>
+        <InvisibleRadio
           type="checkbox"
           name={name}
           id={id}
@@ -36,13 +33,19 @@ const CheckboxField = ({
           }}
           onChange={setValue}
         />
-        <span className="checkmark">
-          <CheckIcon className="checkImage" />
-        </span>
-      </div>
-      <span className="label" onClick={setValue}>
+        <Checkbox className="checkbox">
+          <Image
+            src={'/svg/check.svg'}
+            alt="check-svg"
+            className="check-svg"
+            fill
+            style={{ objectFit: 'cover' }}
+          />
+        </Checkbox>
+      </CheckboxWrapper>
+      <Label className="label" onClick={setValue}>
         {label}
-      </span>
+      </Label>
     </Field>
   );
 };
@@ -51,70 +54,94 @@ export default CheckboxField;
 
 const Field = styled.div`
   display: flex;
-  align-items: flex-start;
+  align-items: center;
   gap: 0.75rem;
   * {
     cursor: pointer;
   }
-  .checkbox-wrapper {
-    position: relative;
-    width: 1rem;
-    height: 1rem;
-  }
+`;
 
-  .radio {
-    position: absolute;
-    top: 0;
-    right: 0;
-    left: 0;
-    bottom: 0;
-    margin: 0;
-    padding: 0;
-    opacity: 0;
-    z-index: 10;
-  }
+const CheckboxWrapper = styled.div`
+  position: relative;
+  width: 1rem;
+  height: 1rem;
 
-  // on click
-  .radio:checked + .checkmark > .checkImage {
+  ${({ theme }) => theme.screen.desktop} {
+    width: 1.25rem;
+    height: 1.25rem;
+  }
+`;
+
+const InvisibleRadio = styled.input`
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  margin: 0;
+  padding: 0;
+  opacity: 0;
+  z-index: 10;
+
+  &:checked + .checkbox > .check-svg {
     opacity: 1;
     border-color: ${({ theme }) => theme.colors.yellow[400]};
     background-color: ${({ theme }) => theme.colors.yellow[400]};
   }
-  .radio:checked + .checkmark {
+
+  &:checked + .checkbox {
     border-color: ${({ theme }) => theme.colors.yellow[400]};
     background-color: ${({ theme }) => theme.colors.yellow[400]};
   }
+`;
 
-  .checkmark {
-    position: absolute;
-    transform: translate(0, -50%);
-    top: 50%;
-    left: 0;
+const Checkbox = styled.span`
+  position: absolute;
+  transform: translate(0, -50%);
+  top: 50%;
+  left: 0;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    border: 1px solid ${({ theme }) => theme.colors.gray[400]};
-    transition: all 100ms ease-in-out;
+  border: 1px solid ${({ theme }) => theme.colors.gray[400]};
+  transition: all 100ms ease-in-out;
 
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 
-    border-radius: 0.25rem;
-  }
+  border-radius: 0.25rem;
 
-  .checkImage {
+  .check-svg {
     transition: all 100ms ease-in-out;
     opacity: 0;
     margin: 0;
   }
 
-  .label {
-    text-align: left;
-    flex: 1;
+  ${({ theme }) => theme.screen.desktop} {
+    .check-svg {
+      width: 1rem !important;
+      height: 1rem !important;
+      top: 2px !important;
+      left: 2px !important;
+    }
+  }
+`;
 
-    font-size: 1rem;
-    color: white;
+const Label = styled.label`
+  text-align: left;
+  flex: 1;
+
+  color: ${({ theme }) => theme.colors.gray[100]};
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.375rem; /* 137.5% */
+  letter-spacing: -0.0375rem;
+
+  ${({ theme }) => theme.screen.desktop} {
+    font-size: 1.25rem;
+    line-height: 1.75rem; /* 155.556% */
+    letter-spacing: -0.0375rem;
   }
 `;
