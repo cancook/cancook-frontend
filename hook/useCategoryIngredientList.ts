97a8b1c87fcdf67ea379becfce45ec3dec3d2d6a
donghakang <dkang0602@gmail.com>
2023-08-12 13:55:50 +0900

    fix: typescript fix in tab section search modal

--- hook/useCategoryIngredientList.ts
+++ hook/useCategoryIngredientList.ts
@@ -1,6 +1,5 @@
 import { getCategoryIngredientList } from '@/apis/search/getCategoryIngredient';
 import { useQuery } from '@tanstack/react-query';
-import { useMemo } from 'react';
 
 // 현재로서는 한번에 ingredient리스트를 전부 받아온다
 export const useCategoryIngredientList = () => {
@@ -9,5 +8,5 @@ export const useCategoryIngredientList = () => {
     getCategoryIngredientList
   );
 
-  return { list: data, ...rest };
+  return { list: data ?? [], ...rest };
 };
