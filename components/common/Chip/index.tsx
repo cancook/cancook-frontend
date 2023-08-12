9b8827aafc76fb89be4fd1c75008ef47c3013d17
donghakang <dkang0602@gmail.com>
2023-08-12 23:18:49 +0900

    feat: chip component

---
+++ components/common/Chip/index.tsx
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled';
+import React from 'react';
+
+const Chip = ({ count }: { count: number }) => {
+  return (
+    <ChipContainer>
+      <span className="chip-counter">{count > 99 ? '99+' : count}</span>
+    </ChipContainer>
+  );
+};
+
+export default Chip;
+
+const ChipContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.125rem 0.5rem;
+  border-radius: 1rem;
+  background-color: ${({ theme }) => theme.colors.yellow[400]};
+
+  .chip-counter {
+    ${({ theme }) => theme.font.label.sm};
+    color: var(--black-white-black, #000);
+  }
+
+  ${({ theme }) => theme.screen.desktop} {
+    padding: 0.125rem 0.725rem;
+
+    .chip-counter {
+      ${({ theme }) => theme.font.headLine.sm};
+      font-size: 1.125rem;
+      font-weight: 700;
+      line-height: 1.75rem;
+    }
+  }
+`;
