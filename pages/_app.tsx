5adf8149fc1e2387a88ca9dbc2ff035df0b07254
Yoon-Hae-Min <yunhatmi@naver.com>
2023-08-14 13:40:46 +0900

    feat: 프러덕트를 GA에 연결

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,20 +4,57 @@ import type { AppProps } from 'next/app';
 import { global } from '@/styles/Global';
 import ReactQueryProvider from '@/provider/ReactQueryProvider';
 import Layout from '@/components/layout/Layout';
+import * as gtag from '@/lib/gtag';
 // Swiper
 import 'swiper/css';
 import Modal from '@/components/common/Modal';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    router.events.on('hashChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+      router.events.off('hashChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
   return (
-    <ReactQueryProvider>
-      <ThemeProvider theme={theme}>
-        <Global styles={global(theme)} />
-        <Modal />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
-    </ReactQueryProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+        }}
+      />
+      <ReactQueryProvider>
+        <ThemeProvider theme={theme}>
+          <Global styles={global(theme)} />
+          <Modal />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ThemeProvider>
+      </ReactQueryProvider>
+    </>
   );
 }
